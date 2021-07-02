a0c25f1f1cd69824a43d60ed743399401f898d70
hoangsxnz <77232866+hoangsxnz@users.noreply.github.com>
2021-07-02 17:26:07 +0700

    Add files via upload

---
+++ src/Collection/ExchangeMoney/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Collection.q1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hoangson
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first currency code: ");
+        String code1 = sc.nextLine();
+        System.out.print("Enter the second currency code: ");
+        String code2 = sc.nextLine();
+        System.out.print("Enter the amount of first currency: ");
+        double amount = sc.nextDouble();
+        Exchange userInput = new Exchange(code1, code2, amount);
+        System.out.print("The amount after converted: ");
+        userInput.convert(code1, code2, amount);
+    }
+}
